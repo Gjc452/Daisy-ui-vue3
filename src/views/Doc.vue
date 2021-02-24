28bfbad8f3e9b67c7b33fd9d489ec6ba03dcf033
Gjc452 <1796444035@qq.com>
2021-02-24 22:43:42 +0800

    整体布局 CSS

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="layout">
+    <Topnav class="nav"/>
     <div class="content">
       <aside v-if='asideVisible'>
         <h2>组件列表</h2>
@@ -40,26 +40,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-aside {
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 
-  > h2 {
-    margin-bottom: 4px;
+  > .nav {
+    flex-shrink: 0;
   }
 
-  > ol {
-    > li {
-      padding: 4px 0;
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    display: flex;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+
+    > aside {
+      flex-shrink: 0;
+      background: lightblue;
+      width: 150px;
+      padding: 70px 16px 16px;
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100%;
+
+      > h2 {
+        margin-bottom: 4px;
+      }
+
+      > ol {
+        > li {
+          padding: 4px 0;
+        }
+      }
+    }
+
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
     }
   }
-
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-  }
 }
+
+main {
+  overflow: auto;
+}
+
 </style>
