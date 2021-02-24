05e90d49bbfc2a39f9b6b2de042ec6c2e8b82ac8
Gjc452 <1796444035@qq.com>
2021-02-24 22:03:51 +0800

    手机界面logo添加 toggleAside 点击事件

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,7 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside">
+    <span class="toggleAside" @click="toggleAside">
 
     </span>
   </div>
@@ -49,15 +49,27 @@ export default {
       margin: 0 1em;
     }
   }
-  > .toggleAside{
 
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
   }
+
   @media (max-width: 500px) {
     > .menu {
       display: none
     }
     > .logo {
       margin: 0 auto
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
