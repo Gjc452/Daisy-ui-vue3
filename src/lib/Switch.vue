16defad14a8792a7a54bad3f8c6d61182e4e637c
Gjc452 <1796444035@qq.com>
2021-02-25 16:21:07 +0800

    CSS 最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,8 +1,7 @@
 <template>
-  <button :class="{checked:value}" @click="toggle">
+  <button class="daisy-switch" :class="{'daisy-checked':value}" @click="toggle">
     <span></span>
   </button>
-  <div>{{ value }}</div>
 </template>
 
 <script lang="ts">
@@ -19,33 +18,16 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.daisy-switch {
   height: $h;
-  width: $h*2;
+  width: $h * 2;
   border: none;
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-
-  &.checked {
-    background: #1890ff;
-
-    > span {
-      left: calc(100% - #{$h2} - 2px);
-    }
-
-    &:active > span {
-      width: $h2 + 4px;
-      margin-left: -4px;
-    }
-  }
-
-  &:active > span {
-    width: $h2 + 4px;
-  }
 
   > span {
     position: absolute;
@@ -54,8 +36,33 @@ button {
     height: $h2;
     width: $h2;
     background: white;
-    border-radius: $h/2;
-    transition: all 0.3s;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+
+  &.daisy-checked {
+    background: #1890ff;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+
+  &.daisy-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
 
