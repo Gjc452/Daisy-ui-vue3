d1e1a5d5fcd414b946b276134da7070e15043f58
Gjc452 <1796444035@qq.com>
2021-02-25 20:28:01 +0800

    使用 Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="daisy-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="daisy-dialog-wrapper">
-      <div class="daisy-dialog">
-        <header>
-          <slot name="title"/>
-          <span class="daisy-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="daisy-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="daisy-dialog-wrapper">
+        <div class="daisy-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="daisy-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
