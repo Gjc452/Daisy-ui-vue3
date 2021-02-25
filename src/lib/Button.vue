de918a6da933a098da630a88884544742728d512
Gjc452 <1796444035@qq.com>
2021-02-25 16:39:59 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -55,6 +55,26 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.daisy-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.daisy-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 
 
