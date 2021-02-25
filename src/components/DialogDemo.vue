cb682ce965638bc6fb35e28dcd3a2e91b7f78775
Gjc452 <1796444035@qq.com>
2021-02-25 20:14:14 +0800

    完成 Dialog 点击关闭功能

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,29 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible" :close-onclick-overlay="false"
+          :ok="f1" :cancel="f2"
+  ></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Dialog}
+  components: {Button, Dialog},
+  setup() {
+    const visible = ref(false);
+    const toggle = () => {
+      visible.value = !visible.value;
+    };
+    const f1 = () => {
+      return true;
+    };
+    const f2 = () => {};
+    return {visible, toggle, f1, f2};
+  }
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,27 +1,65 @@
 <template>
-  <div class="daisy-dialog-overlay"></div>
-  <div class="daisy-dialog-wrapper">
-    <div class="daisy-dialog">
-      <header>标题
-        <span class="daisy-dialog-closel"></span>
-      </header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="daisy-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="daisy-dialog-wrapper">
+      <div class="daisy-dialog">
+        <header>标题
+          <span class="daisy-dialog-close" @click="close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
-  components: {Button}
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    closeOnclickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function
+    },
+    cancel: {
+      type: Function
+    }
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnclickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+    return {close, onClickOverlay, ok, cancel};
+  }
 };
 </script>
 
