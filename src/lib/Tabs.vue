8e3d500356ef29cf3ff5b4fada80fbebff9da20d
Gjc452 <1796444035@qq.com>
2021-02-25 23:39:43 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,7 +3,7 @@
     <div class="daisy-tabs-nav" ref="container">
       <div class="daisy-tabs-nav-item" v-for="(t,index) in titles"
            :class="{selected:t===selected}" @click="select(t)" :key="index"
-           :ref="el=>{if(el) navItems[index]=el}">
+           :ref="el=>{if(t ===selected) selectedItem = el}">
         {{ t }}
       </div>
       <div class="daisy-tabs-nav-indicator" ref="indicator"></div>
@@ -25,17 +25,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div =>
-          div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     };
@@ -58,7 +55,7 @@ export default {
       context.emit('update:selected', title);
     };
     return {
-      defaults, titles, current, select, navItems, indicator, container
+      defaults, titles, current, select, indicator, container, selectedItem
     };
   }
 };
