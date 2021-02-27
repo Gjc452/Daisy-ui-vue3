8742dd3b7ed461be53baf9d56ba1ad050b51de8a
Gjc452 <1796444035@qq.com>
2021-02-27 21:40:24 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -10,8 +10,8 @@ export default {
     globals: {
       vue: 'Vue'
     },
-    name: 'Gulu',
-    file: 'dist/lib/gulu.js',
+    name: 'Wish',
+    file: 'dist/lib/Wish.js',
     format: 'umd',
     plugins: [terser()]
   },
